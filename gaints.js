b918e512aea70c306763191cd1ee738103f4b65d
SoftwareDev-EzinneO <esthyonuoha10@gmail.com>
2026-02-12 16:07:51 +0100

    my first commit work

---
+++ gaints.js
@@ -0,0 +1,10 @@
+// Alert statement
+
+let Name = "Zinnie";
+    age = 25,
+    Status = "single",
+    height = 5.5,
+    position = "software Developer"
+    message = "hi eveyone";
+
+    alert(`${message} my name is ${Name} i am ${age} years old i am ${Status} and i am ${height} working as a ${position}`)
